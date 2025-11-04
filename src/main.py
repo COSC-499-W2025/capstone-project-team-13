dea2f6032a64c33aa181ce905b9bcf1f4dde2d58
JacksonWilson99 <113214700+JacksonWilson99@users.noreply.github.com>
2025-11-04 13:11:25 -0800

    main.py updated

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from src.Helpers.installDependencies import install_requirements
+install_requirements()
 from src.Analysis import codeIdentifier, visualMediaAnalyzer
 from src.UserPrompts.getConsent import get_user_consent
 from src.Helpers.fileFormatCheck import check_file_format, InvalidFileFormatError
