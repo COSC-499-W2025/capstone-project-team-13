65a271fa2b0ee93c5ac27c6bb6e282bda3cf39fe
sshah-git <its.sanashah@gmail.com>
2026-02-19 11:41:46 -0800

    updated mainAPI.py

--- src/mainAPI.py
+++ src/mainAPI.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from src.Routers import projects
+from src.Routers import projects, skills
 
 app = FastAPI(title="Digital Artifact Mining API")
 app.include_router(projects.router)
+app.include_router(skills.router)
 
